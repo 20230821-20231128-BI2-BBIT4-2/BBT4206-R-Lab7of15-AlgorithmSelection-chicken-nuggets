dca74095d3940687fa5244e9812be030943cabfe
BirBhambra <birkanwal.bhambra@strathmore.edu>
2023-10-29 23:59:09 +0300

    Added association

--- Lab7-Submission-AlgorithmSelection.R
+++ Lab7-Submission-AlgorithmSelection.R
@@ -328,6 +328,206 @@ fourfoldplot(as.table(confusion_matrix), color = c("grey", "lightblue"),
              main = "Confusion Matrix")
 
 
+# ASSOCIATION ----
+# STEP 1. Install and Load the Required Packages ----
+## arules ----
+if (require("arules")) {
+  require("arules")
+} else {
+  install.packages("arules", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## arulesViz ----
+if (require("arulesViz")) {
+  require("arulesViz")
+} else {
+  install.packages("arulesViz", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## tidyverse ----
+if (require("tidyverse")) {
+  require("tidyverse")
+} else {
+  install.packages("tidyverse", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## readxl ----
+if (require("readxl")) {
+  require("readxl")
+} else {
+  install.packages("readxl", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## knitr ----
+if (require("knitr")) {
+  require("knitr")
+} else {
+  install.packages("knitr", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## ggplot2 ----
+if (require("ggplot2")) {
+  require("ggplot2")
+} else {
+  install.packages("ggplot2", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## lubridate ----
+if (require("lubridate")) {
+  require("lubridate")
+} else {
+  install.packages("lubridate", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## plyr ----
+if (require("plyr")) {
+  require("plyr")
+} else {
+  install.packages("plyr", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## dplyr ----
+if (require("dplyr")) {
+  require("dplyr")
+} else {
+  install.packages("dplyr", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## naniar ----
+if (require("naniar")) {
+  require("naniar")
+} else {
+  install.packages("naniar", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## RColorBrewer ----
+if (require("RColorBrewer")) {
+  require("RColorBrewer")
+} else {
+  install.packages("RColorBrewer", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+# STEP 2. Load and pre-process the dataset ----
+## FORMAT 1: Single Format ----
+# An example of the single format transaction data is presented
+# here: "data/transactions_single_format.csv" and loaded as follows:
+transactions_single_format <-
+  read.transactions("data/transactions_single_format.csv",
+                    format = "single", cols = c(1, 2))
+
+View(transactions_single_format)
+print(transactions_single_format)
+
+# Reading the set
+retail <- read_excel("data/GroceryStoreDataSet.csv")
+dim(retail)
+
+### Handle missing values ----
+# Are there missing values in the dataset?
+any_na(retail)
+
+# How many?
+n_miss(retail)
+
+# What is the proportion of missing data in the entire dataset?
+prop_miss(retail)
+
+# What is the number and percentage of missing values grouped by
+# each variable?
+miss_var_summary(retail)
+
+# Which variables contain the most missing values?
+gg_miss_var(retail)
+
+# Which combinations of variables are missing together?
+gg_miss_upset(retail)
+
+#### Removing the observations with missing values ----
+retail_removed_obs <- retail %>% filter(complete.cases(.))
+
+# We end up with 406,829 observations to create the association rules
+# instead of the initial 541,909 observations.
+dim(retail_removed_obs)
+
+# Are there missing values in the dataset?
+any_na(retail_removed_obs)
+
+## Record only the `items` variable ----
+# Notice that at this point, the single format ensures that each transaction is
+# recorded in a single observation. We therefore no longer require the
+# `invoice_no` variable and all the other variables in the data frame except
+# the itemsets.
+
+### OPTION 1 ----
+transaction_data <-
+  transaction_data %>%
+  dplyr::select("items" = V1)
+#  %>% mutate(items = paste("{", items, "}", sep = ""))
+
+View(transaction_data)
+
+## Save the transactions in CSV format ----
+### OPTION 1 ----
+write.csv(transaction_data,
+          "data/transactions_basket_format_grocery_store.csv",
+          quote = FALSE, row.names = FALSE)
+
+## Read the transactions from the CSV file ----
+### OPTION 1 ----
+tr <-
+  read.transactions("data/transactions_basket_format_grocery_store.csv",
+    format = "basket",
+    header = TRUE,
+    rm.duplicates = TRUE,
+    sep = ","
+  )
+
+print(tr)
+summary(tr)
+
+# STEP 3. Create the association rules ----
+association_rules_prod_name <- apriori(tr,
+                                       parameter = list(support = 0.01,
+                                                        confidence = 0.8,
+                                                        maxlen = 10))
+
+# STEP 3. Print the association rules ----
+## OPTION 1 ----
+# Threshold values of support = 0.01, confidence = 0.8, and
+# maxlen = 10 results in a total of 83 rules when using the
+# stock code to identify the products.
+summary(association_rules)
+inspect(association_rules)
+# To view the top 10 rules
+inspect(association_rules[1:10])
+plot(association_rules)
+
+### Remove redundant rules ----
+# We can remove the redundant rules as follows:
+subset_rules <-
+  which(colSums(is.subset(association_rules,
+                          association_rules)) > 1)
+length(subset_rules)
+association_rules_no_reps <- association_rules[-subset_rules]
+
+# This results in 40 non-redundant rules (instead of the initial 83 rules)
+summary(association_rules_no_reps)
+inspect(association_rules_no_reps)
+
+write(association_rules_no_reps,
+      file = "rules/association_rules_grocery.csv")
+
 
 
 
